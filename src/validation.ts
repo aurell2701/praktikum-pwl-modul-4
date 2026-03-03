d6c4b22552e61eec516511fd05cf49d3c0d7a32f
Aurellya <h1101241043@student.untan.ac.id>
2026-03-03 12:51:04 +0700

    update new

--- src/validation.ts
+++ src/validation.ts
@@ -1,8 +1,8 @@
+// PRAKTIKUM 1 - validasi request body
 import { Elysia, t } from "elysia";
 import { openapi } from "@elysiajs/openapi";
 
 const app = new Elysia()
-// PRAKTIKUM 1 - validasi request body
   .use(openapi())
   .post("/request",
     ({ body }) => {
@@ -20,41 +20,6 @@ const app = new Elysia()
     }
   )
   .listen(3000);
-app.get(
-  "/search",
-  ({ query }) => query,
-  {
-    query: t.Object({
-      keyword: t.String(),
-      page: t.Optional(t.Number())
-    })
-  }
-)
 
-app.get(
-  "/user/:id",
-  ({ params }) => params,
-  {
-    params: t.Object({
-      id: t.Number()
-    })
-  }
-)
-
-  app.get(
-  "/ping",
-  () => {
-    return {
-      success: true,
-      message: "Server OK"
-    }
-  },
-  {
-    response: t.Object({
-      success: t.Boolean(),
-      message: t.String()
-    })
-  }
-)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
